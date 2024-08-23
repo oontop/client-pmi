41b50bbf7d040f533e6ac1c3da82619446720222
idan.o <Idan.o@solaredge.com>
2024-08-23 15:14:31 +0300

    add date for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className=" md:px-14 p-4 max-w-screen-2xl mx-auto dark:text-white mt-2  ">
             <hr />
-            <p>© 2024 Eliyahu Levi. All rights reserved. </p>
+            <p>© {(new Date()).getFullYear()} <a href="https://www.oontop.com/" target="_blank">Oontop-Helath</a>. All rights reserved.</p>
         </div>
     )
 }
