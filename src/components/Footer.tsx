b94c57765cf7a9d1b2d4c221a9d1c05d03941707
idan.o <Idan.o@solaredge.com>
2024-08-23 15:15:07 +0300

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div className=" md:px-14 p-4 max-w-screen-2xl mx-auto dark:text-white mt-2  ">
             <hr />
+            {/* add here link to onntop helath */}
             <p>© {(new Date()).getFullYear()} <a href="https://www.oontop.com/" target="_blank">Oontop-Helath</a>. All rights reserved.</p>
         </div>
     )
